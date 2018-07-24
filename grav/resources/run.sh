037b05d9b76c4207fd796d99b81ad632da446727
icebear8 <ebert.dominik@gmail.com>
2018-07-24 21:25:44 +0200

    If there is a git repository given, replace the content with the content from the git repository

--- grav/resources/run.sh
+++ grav/resources/run.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 
+if ! [[ -z "${CONTENT_REPO_URL}" ]]; then
+  rm -rf ${APP_DIR}/user/pages/*
+  git clone --depth 1 ${CONTENT_REPO_URL} ${APP_DIR}/user/
+fi
+
 php-fpm5
 nginx -g "daemon off;"
