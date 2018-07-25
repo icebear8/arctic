51e799feb1641d07544d8d8db05b48a536371d47
icebear8 <ebert.dominik@gmail.com>
2018-07-25 20:33:36 +0200

    Move launcher script and introduce utils directory

--- grav/utils/launcher.sh
+++ grav/utils/launcher.sh
@@ -3,7 +3,7 @@
 if ! [[ -z "${CONTENT_REPO_URL}" ]]; then
   git clone --depth 1 ${CONTENT_REPO_URL} ${CONTENT_DIR}
   rm -rf ${APP_DIR}/grav/user
-  ln -s ${CONTENT_DIR}/public/user ${APP_DIR}/grav/user
+  ln -s ${CONTENT_DIR}/web/user ${APP_DIR}/grav/user
 fi
 
 php-fpm5
