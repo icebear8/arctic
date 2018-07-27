d5d35971b88872a8f10fb13a94b69d4dac294f64
icebear8 <ebert.dominik@gmail.com>
2018-07-27 23:34:05 +0200

    Workaround: From PHP and triggered by nginx, env variables from host do not work

--- grav/resources/deploy.php
+++ grav/resources/deploy.php
@@ -2,7 +2,7 @@
 ignore_user_abort(true);
 set_time_limit(0);
 
-$script       = '${UTILS_DIR}/updateContent.sh';
+$script       = '/var/www/utils/updateContent.sh';
 $dstLog       = '/var/log/updateContent.log';
 
 shell_exec('sh '.$script.' >> '.$dstLog.' 2>&1');
--- grav/utils/updateContent.sh
+++ grav/utils/updateContent.sh
@@ -1,16 +1,13 @@
 #!/bin/sh
 
-if ! [[ -z "${CONTENT_REPO_URL}" ]]; then
-  # Only update if there is a repository available
-  echo "Start update"
-  date
+# Only update if there is a repository available
+echo "Start update"
+date
 
-  cd ${CONTENT_DIR}
-  git pull --rebase origin master
+cd /var/www/content
+git pull --rebase origin master
 
-  # Wait for all processes to be finished
-  wait
+# Wait for all processes to be finished
+wait
 
-  echo "Update done"
-
-fi
+echo "Update done"
