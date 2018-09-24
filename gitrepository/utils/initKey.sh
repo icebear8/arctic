85d3998ea8ccd3e7e680ee99ac1e3a71b5b05468
icebear8 <ebert.dominik@gmail.com>
2018-09-24 22:29:34 +0200

    Gitrepository: Strip down and create a base image for re usage

--- gitrepository/utils/initKey.sh
+++ gitrepository/utils/initKey.sh
@@ -1,8 +1,18 @@
 #!/bin/sh
 
+# Prepare known hosts if not existing
+if [ -f ~/.ssh/known_hosts ]; then
+  for f in /opt/utils/known_hosts*
+  do
+    cat $f > ~/.ssh/known_hosts
+  done
+fi
+
+# Remove keys if they exist
 if [ -f ~/.ssh/id_rsa ]; then
   rm ~/.ssh/id_rsa
   rm ~/.ssh/id_rsa.pub
 fi
 
+# Generate ad hoc keys for authentication
 ssh-keygen -f ~/.ssh/id_rsa -q -P ""
