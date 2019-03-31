73830c8f80b727478bdebc47b845ad308cf0ce41
cheshire.lynx <cheshire.lynx@protonmail.com>
2019-03-31 17:43:49 +0200

    Refactor: Tab intent and error reporting

--- denonRemoteControl/DenonControlService/DenonControlService/DenonControlService.py
+++ denonRemoteControl/DenonControlService/DenonControlService/DenonControlService.py
@@ -10,66 +10,66 @@ remote = None
 restService = None
 
 def _startServices():
-    global remote
-    global restService
+  global remote
+  global restService
 
-    logging.debug("Create remote connection")
+  logging.debug("Create remote connection")
 
-    remote = RemoteConnection()
+  remote = RemoteConnection()
 
-    logging.debug("Create REST service")
-    restService = RestService()
-    RestService.remoteConnection = remote
-    restService.start()
+  logging.debug("Create REST service")
+  restService = RestService()
+  RestService.remoteConnection = remote
+  restService.start()
 
 def _runAsService():
-    logging.info("Run as service")
-    while True:
-        time.sleep(10)
+  logging.info("Run as service")
+  while True:
+    time.sleep(10)
 
 def _runAsConsole():
-    logging.info("Run as console application")
+  logging.info("Run as console application")
 
-    time.sleep(1)
-    print("Press any key to exit...")
-    input()
-    _stopServices()
+  time.sleep(1)
+  print("Press any key to exit...")
+  input()
+  _stopServices()
 
 def _stopServices():
-    logging.info("Stopping services")
-    remote.disconnect()
-    restService.stop()
+  logging.info("Stopping services")
+  remote.disconnect()
+  restService.stop()
 
 def _initializeLogging():
-    logging.basicConfig(format='%(asctime)s %(levelname)s: %(message)s', datefmt='%Y-%m-%d %I:%M:%S %p', level=logging.INFO)
-    logging.Formatter.converter = time.gmtime
+  logging.basicConfig(format='%(asctime)s %(levelname)s: %(message)s', datefmt='%Y-%m-%d %I:%M:%S %p', level=logging.INFO)
+  logging.Formatter.converter = time.gmtime
 
 def main(argv):
-    isService=False
-    
-    _initializeLogging()
-    logging.info("Main started")
+  isService=False
   
-    try:
-        opts, args = getopt.getopt(argv, "s")
-    except getopt.GetoptError as err:
-        logging.error(err)  # will print something like "option -a not recognized"
-        # print help information and exit:
-        loggin.info('DenonControlService.py [-s]')
-        sys.exit(2)
-        
-    for opt, arg in opts:
-        if opt == '-s':
-            isService=True
+  _initializeLogging()
+  logging.info("Main started")
+  
+  try:
+    opts, args = getopt.getopt(argv, "s")
+  except getopt.GetoptError as err:
+    print(err)  # will print something like "option -a not recognized"
+    # print help information and exit:
+    print('DenonControlService.py [-s]')
+    sys.exit(2)
+      
+  for opt, arg in opts:
+    if opt == '-s':
+      isService=True
 
-    _startServices()
+  _startServices()
 
-    if isService is True:
-        _runAsService()
-    else:
-        _runAsConsole()
-    
+  if isService is True:
+    _runAsService()
+  else:
+    _runAsConsole()
+  
 if __name__ == '__main__':
-    main(sys.argv[1:])
+  main(sys.argv[1:])
 
 
