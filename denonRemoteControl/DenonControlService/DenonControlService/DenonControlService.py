6dd27e3ff8130ba6d92e6fe8bce1717218c3de65
cheshire.lynx <cheshire.lynx@protonmail.com>
2019-04-06 17:30:34 +0200

    DenonControlService
    - Expose arguments (loglevel and inactivity timeout)
    - Only allow start as a service

--- denonRemoteControl/DenonControlService/DenonControlService/DenonControlService.py
+++ denonRemoteControl/DenonControlService/DenonControlService/DenonControlService.py
@@ -9,55 +9,52 @@ from RestService import RestService
 remote = None
 restService = None
 
-def _startServices(host):
+def _runServices(targetHost, timeout):
   global remote
   global restService
 
   logging.debug("Create remote connection")
 
-  remote = RemoteConnection(host)
+  remote = RemoteConnection(host=targetHost)
+  
+  if timeout not None:
+    remote.inactivityTimeoutSec = timeout
 
   logging.debug("Create REST service")
   restService = RestService()
   RestService.remoteConnection = remote
   restService.start()
 
-def _runAsService():
-  logging.info("Run as service")
   while True:
     time.sleep(10)
-
-def _runAsConsole():
-  logging.info("Run as console application")
-
-  time.sleep(1)
-  print("Press any key to exit...")
-  input()
-  _stopServices()
 
 def _stopServices():
   logging.info("Stopping services")
   remote.disconnect()
   restService.stop()
 
-def _initializeLogging():
-  logging.basicConfig(format='%(asctime)s %(levelname)s: %(message)s', datefmt='%Y-%m-%d %I:%M:%S %p', level=logging.INFO)
+def _initializeLogging(loglevel):
+  numeric_level = getattr(logging, loglevel.upper(), None)
+  if not isinstance(numeric_level, int):
+    numeric_level = getattr(logging, INFO, None)
+
+  logging.basicConfig(format='%(asctime)s %(levelname)s: %(message)s', datefmt='%Y-%m-%d %I:%M:%S %p', level=numeric_level)
   logging.Formatter.converter = time.gmtime
 
 def _printUsage():
-  print('DenonControlService.py [-s --host=]')
-  print('-s: Start as service')
+  print('DenonControlService.py [--host=, --log=, --timeout=]')
   print('--host=: Host name or IP to connect')
+  print('--log=: Loglevel [DEBUG, INFO, WARNING, ERROR, CRITICAL]')
+  print('--timeout=: Inactivity disconnection timeout in seconds (default=300 sec)')
   
 def main(argv):
-  isService=False
-  host=""
-  
-  _initializeLogging()
-  logging.info("Main started")
+  isService = False
+  host = ""
+  loglevel = ""
+  timeout = None
   
   try:
-    opts, args = getopt.getopt(argv, "hs", ["help", "host="])
+    opts, args = getopt.getopt(argv, "h", ["help", "host=", "log=", "timeout="])
   except getopt.GetoptError as err:
     print(err)  # will print something like "option -a not recognized"
     _printUsage()
@@ -67,17 +64,18 @@ def main(argv):
     if opt in ('-h', '--help'):
       _printUsage()
       sys.exit()
-    elif opt in ('-s'):
-      isService=True
     elif opt in ('--host'):
-      host=arg
+      host = arg
+    elif opt in ('--log='):
+      loglevel = arg
+    elif opt in ('--timeout='):
+      timeout = arg
+    
 
-  _startServices(host)
+  _initializeLogging(loglevel)
+  logging.info("Main started")
 
-  if isService is True:
-    _runAsService()
-  else:
-    _runAsConsole()
+  _runServices(host, timeout)
   
 if __name__ == '__main__':
   main(sys.argv[1:])
